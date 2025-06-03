405acdfd1db9fa52a6a8633079e05c1ee0592e69
nyakeriga <122856235+nyakeriga@users.noreply.github.com>
2025-06-03 20:27:02 +0300

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,10 +1,28 @@
-// Sample ViewController.swift
 import UIKit
+import NetworkExtension
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        // Add UI elements here
+        configureVPN()
+    }
+
+    func configureVPN() {
+        let manager = NETunnelProviderManager()
+        let config = NETunnelProviderProtocol()
+        config.providerBundleIdentifier = "com.example.iosvpn.PacketTunnel"
+        config.serverAddress = "SingBox"
+        manager.protocolConfiguration = config
+        manager.localizedDescription = "SingBox VPN"
+        manager.isEnabled = true
+
+        manager.saveToPreferences { error in
+            if let error = error {
+                print("Error saving VPN: \(error)")
+            } else {
+                print("VPN configuration saved")
+            }
+        }
     }
 }
