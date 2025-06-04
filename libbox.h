32cc3c66d884c33b8ef20d47dc72ed1207ca1c68
nyakeriga <122856235+nyakeriga@users.noreply.github.com>
2025-06-04 19:07:14 +0300

    Create libbox.h

---
+++ libbox.h
@@ -0,0 +1,16 @@
+// libbox.h
+#ifndef LIBBOX_H
+#define LIBBOX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int singbox_main(int argc, char **argv);
+void singbox_exit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LIBBOX_H
