a86b7e4d60fe6bb350ccad7a33f4a969b2dd7f9a
nyakeriga <122856235+nyakeriga@users.noreply.github.com>
2025-06-05 18:28:44 +0300

    Update wrapper.go

--- wrapper.go
+++ wrapper.go
@@ -1,15 +1,39 @@
-// wrapper.go
 package main
 
 import "C"
+
 import (
-    "fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/sagernet/sing-box"
 )
 
 //export StartSingBox
 func StartSingBox() {
-    fmt.Println("Sing-box running in iOS")
-    // Add Sing-box logic here if needed
+	// Locate config in app bundle (iOS)
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("getwd failed: %v", err)
+	}
+
+	configPath := filepath.Join(dir, "config.json")
+	log.Printf("Loading config: %s", configPath)
+
+	engine, err := singbox.New(singbox.Options{
+		ConfigPath: configPath,
+	})
+	if err != nil {
+		log.Fatalf("failed to create engine: %v", err)
+	}
+
+	err = engine.Start()
+	if err != nil {
+		log.Fatalf("failed to start engine: %v", err)
+	}
+
+	log.Println("Sing-box started")
 }
 
 func main() {}
