bdeaad90da643367b2f40e502e7560202910e747
nyakeriga <122856235+nyakeriga@users.noreply.github.com>
2025-06-06 19:54:20 +0300

    Update main.go

--- wrapper/main.go
+++ wrapper/main.go
@@ -1,4 +1,24 @@
-service, err = libbox.NewService(cfgPath, &iosPlatform{}) // or dummyPlatform{}
+package main
+
+import (
+	"log"
+
+	"github.com/nyakeriga/singbox-wrapper/platforminterface"
+	"github.com/sagernet/sing-box/experimental/libbox"
+)
+
+func main() {
+	cfgPath := "/path/to/config.json" // Replace this with the actual path in iOS/macOS context
+
+	service, err := libbox.NewService(cfgPath, &platforminterface.IOSPlatform{})
+	if err != nil {
+		log.Fatalf("Failed to create service: %v", err)
+	}
+
+	if err := service.Start(); err != nil {
+		log.Fatalf("Service start failed: %v", err)
+	}
+}
 
 
 
